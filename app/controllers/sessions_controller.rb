94b9d850ae0a09494c0e76021ff23071d700e82b
tmk616 <tmk616window@gmail.com>
2020-04-06 20:54:31 +0000

    finish login_user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @user = User.find_by(email: session_params[:email])
+    
+    if @user&.authenticate(session_params[:password])
+     session[:user_id] = @user.id
+      flash[:success]= 'ログインしました' 
+     redirect_to root_url
+    else
+      flash[:danger] = 'メールアドレスかパスワードが間違っています' 
+      
+      render :new
+    end
+  end
+  
+  
+  private
+  
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
+  
+end
